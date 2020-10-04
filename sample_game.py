f27fc1564d0b83771bdd1e97c25d62f592c19691
Chandreogwda <valmiki20@protonmail.com>
2020-10-04 00:52:26 -0500

    check

--- sample_game.py
+++ sample_game.py
@@ -10,12 +10,23 @@ player_img = pygame.image.load("pacman.png")
 player_x = 300
 player_y = 500
 
-def player():
-    screen.blit(player_img,(player_x,player_y))
+def player(x,y):
+    screen.blit(player_img,(x,y))
 while running:
     for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.type is pygame.K_a:
+                print("A is pressed")
+            if event.type is pygame.K_d:
+                print("Left is pressed")
+                player_x -= 5
+            if event.type is pygame.K_a:
+                print("Right is pressed")
+                player_x += 5
+            else:
+                print("Nothing pressed"+)
         if event.type == pygame.QUIT:
             running= False
         screen.fill((0,0,0))
-        player()
+        player(player_x,player_y)
         pygame.display.update()
