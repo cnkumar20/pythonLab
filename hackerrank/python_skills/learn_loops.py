efebdfcc65b876b02c666847578e45b4961dfc33
Nandiesh Kumar Chandregowda <nchandregowda@conversantmedia.com>
2020-10-07 23:16:51 -0500

    python skills

---
+++ hackerrank/python_skills/learn_loops.py
@@ -0,0 +1,8 @@
+def list_squares_of_list_less_than_n(n):
+    for x in range(n):
+        print(x*x)
+
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    list_squares_of_list_less_than_n()
